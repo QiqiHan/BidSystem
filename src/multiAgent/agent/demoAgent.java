3f4bd9cdbec2577f855ce39fd4957d8fbffb14c5
hqq <631733812@qq.com>
2017-05-04 20:27:16 +0800

    h77到此一游

--- src/multiAgent/agent/demoAgent.java
+++ src/multiAgent/agent/demoAgent.java
@@ -1,24 +1,19 @@
 package multiAgent.agent;
 
-import jade.content.*;
 import jade.core.Agent;
-import jade.core.behaviours.*;
-import jade.lang.acl.*;
+import jade.core.behaviours.Behaviour;
+import multiAgent.behavior.demoBehaviour;
 
 /**
  * Created by H77 on 2017/5/4.
  */
 public class demoAgent extends Agent {
 
-    Behaviour oneShot = new OneShotBehaviour(this) {
-        @Override
-        public void action() {
-            System.out.println("hello Agent");
-        }
-    };
+
 
     protected void setup(){
-        addBehaviour(oneShot);
+        Behaviour demo = new demoBehaviour();
+        addBehaviour(demo);
     }
 
 
