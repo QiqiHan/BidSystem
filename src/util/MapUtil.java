7a1ae14e1ecee458d70081dd19a3d247f51ecf73
mac <465033064@qq.com>
2017-05-16 13:44:30 +0800

    根据地址查询经纬度

--- src/util/MapUtil.java
+++ src/util/MapUtil.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -12,6 +13,7 @@ import org.apache.http.util.EntityUtils;
 import util.MapObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.*;
 
@@ -28,7 +30,7 @@ public class MapUtil {
         for(String keyword : keyWords) {
             //得到距离最近的10个关键词地点,如果没有map中就对应一个，暂时是1km为半径的圆
             List<MapObject> obs = new ArrayList<MapObject>();
-            String jsonString = httpGet(keyword, longitude, lat);
+            String jsonString = aroundSiteGet(keyword, longitude, lat);
             JSONObject jsonObject = JSON.parseObject(jsonString);
             if ((Integer) jsonObject.get("total") == 0) {
                 jsonObjects.put(keyword, obs);
@@ -57,7 +59,45 @@ public class MapUtil {
         return jsonObjects;
     }
 
-    private static String httpGet(String keyWord , String longitude ,String lat){
+    //经纬度转化
+    public static MapObject convert(String location){
+        String url = "http://api.map.baidu.com/geocoder/v2/?address=";
+        url  = url + location + "&output=json&ak=sqm5UqFMLzcb9XVx2baZVX3berIcLnEq";
+        MapObject mapObject = new MapObject();
+        try {
+            url = URLDecoder.decode(url, "UTF-8");
+            HttpClient client = new DefaultHttpClient();
+            //发送get请求
+            HttpGet request = new HttpGet(url);
+            HttpResponse response = client.execute(request);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                String strResult = EntityUtils.toString(response.getEntity());
+                JSONObject jsonObject = JSON.parseObject(strResult);
+                JSONObject result = jsonObject.getJSONObject("result");
+                JSONObject loca = result.getJSONObject("location");
+                mapObject.setLng(loca.getString("lng"));
+                mapObject.setLat(loca.getString("lat"));
+                System.out.println(mapObject);
+                return mapObject;
+            } else {
+                System.out.println("请求失败");
+                return null ;
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //查询周边用
+    private static String aroundSiteGet(String keyWord , String longitude ,String lat){
         String url = "http://api.map.baidu.com/place/v2/search?query=";
         url=url+keyWord+"&location="+lat+","+longitude+"&radius=1000&filter=sort_name:distance%7csort_rule:1&output=json&page_num=0&scope=2&ak=sqm5UqFMLzcb9XVx2baZVX3berIcLnEq";
         try {
@@ -81,10 +121,11 @@ public class MapUtil {
     }
 
     public static  void main(String[] args){
-        List<String> keywords = new ArrayList<String>();
-        keywords.add("公交");
-        keywords.add("旅游景点");
-        searchAroundSite(keywords,"120.90407","30.944314");
+//        List<String> keywords = new ArrayList<String>();
+//        keywords.add("公交");
+//        keywords.add("旅游景点");
+//        searchAroundSite(keywords,"120.90407","30.944314");
+          convert("西塘");
     }
 }
 
